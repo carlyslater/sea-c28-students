48b93e88e77f73dd237887776746e9d140621596
Carly Slater <carlyslater@gmail.com>
2014-12-06 15:02:49 -0800

    rot13 now with code tthat works!  doesn't have testing function yet.  that's next

--- Students/CarlySlater/rot13.py
+++ Students/CarlySlater/rot13.py
@@ -1,1 +1,14 @@
-rot13 = str.translate(table[abcdefghijklmABCDEFGHIJKLMnopqrstuvwxyzNOPQRSTUVWXYZ, nopqrstuvwxyzNOPQRSTUVWXYZabcdefghijklmABCDEFGHIJKLM])
+import string
+
+
+rot13 = string.maketrans("abcdefghijklmABCDEFGHIJKLMnopqrstuvwxyzNOPQRSTUVWXYZ", "nopqrstuvwxyzNOPQRSTUVWXYZabcdefghijklmABCDEFGHIJKLM")
+
+def translateytimes():
+    phrase = string.translate((raw_input("enter phrase for translation    ")), rot13)
+    print phrase
+
+translateytimes()
+
+
+#if __name__ == '__main__'
+#should ask for input and translate it.
