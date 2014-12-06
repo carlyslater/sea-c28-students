2fc4b49e9681b670f3f6db0bbb8d0dd2c53598ab
Carly Slater <carlyslater@gmail.com>
2014-12-06 15:41:36 -0800

    added mlrmmadPLAN.py which is the pseudo code and planning doc for the mailroom madness assignment

---
+++ Students/CarlySlater/mlrmmadPLAN.py
@@ -0,0 +1,19 @@
+def Namer():
+Full_Name = raw_input("Please provide Donor name ")
+while Full_Name == "exit":
+break
+while Full_Name == "list":
+print {donors}
+Full_Name = raw_input("Please provide Donor name ")
+
+while Full_Name is not on {donors}:
+    {donors}.ammend(Full_Name)
+Insert_Name = Full_Name
+#easier to call a new thing than keep returning 
+def Donationer():
+Donation_Amount = raw_input("Please provide Donation Amount")
+while Donation_Amount != a float:
+print "Please enter a numeric value"
+Donation_Amount = raw_input("Please provide Donation Amount")
+{donation}.ammend(Donation_Amount)
+Donation_Insert = Donation_Amount
